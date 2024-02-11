74fc0973e1c65c46ca1953e278d013473f8034d6
yakir <158854050+YakirDray@users.noreply.github.com>
2024-02-11 22:31:18 +0200

    navigator.login

--- CafeQueueReduce/app/(authenticate)/_layout.js
+++ CafeQueueReduce/app/(authenticate)/_layout.js
@@ -1,14 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {Stack} from "expo-router";
+export default function _layout(){
 
-const _layout = () => {
   return (
-    <View>
-      <Text>_layout</Text>
-    </View>
+   
+<Stack>
+  <Stack.Screen name="login" options={{headerShown:false}}/>
+
+</Stack>
   )
 }
 
-export default _layout
-
-const styles = StyleSheet.create({})
