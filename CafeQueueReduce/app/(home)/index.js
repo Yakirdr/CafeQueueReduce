d161acc0da9f3bb714edf5457849bc9328d07647
yakir <158854050+YakirDray@users.noreply.github.com>
2024-02-11 23:57:08 +0200

    fix

--- CafeQueueReduce/app/(home)/index.js
+++ CafeQueueReduce/app/(home)/index.js
@@ -1,23 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import Carousal from '../../Components/Carousal';
-import Comp1 from '../../Components/comp1';
-import Comp2 from'../../Components/comp2';
-import Menu from '../../Components/menu';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 const index = () => {
   return (
     <View>
       <Text>home screen</Text>
-      <View>
-   <Comp1/>
-      </View>
-      <Comp2/>
-      <View>
-<Menu/>
-      </View>
-<Carousal/>
-
     </View>
   )
 }
