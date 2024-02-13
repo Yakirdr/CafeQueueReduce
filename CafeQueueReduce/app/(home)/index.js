2a96ed1ac7b9cc4df58b0c3c9fab6cc887c83030
yakir <158854050+YakirDray@users.noreply.github.com>
2024-02-13 08:11:34 +0200

    apdate s&R

--- CafeQueueReduce/app/(home)/index.js
+++ CafeQueueReduce/app/(home)/index.js
@@ -4,7 +4,7 @@ import Carousel from "../../Components/Carousal";
 const index = () => {
   return (
     <View>
-      <Text>home s </Text>
+      <Text>home screen please dun!</Text>
     <Carousel/>
     </View>
   )
