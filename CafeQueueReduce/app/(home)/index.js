ff774f3d3247ba9291de453a8e758e91dbe28ec3
ofekp3 <ofekpe@ac.sce.ac.il>
2024-03-03 13:34:25 +0200

    ofek

--- CafeQueueReduce/app/(home)/index.js
+++ CafeQueueReduce/app/(home)/index.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Carousel from "../../Components/Carousal";
+import Mmenu from "../../Components/mainmenu";
 const index = () => {
   return (
     <View>
-      <Text>home screen please dun!</Text>
+      <Text>home screen please done!</Text>
     <Carousel/>
+   <Mmenu/>
     </View>
   )
 }
