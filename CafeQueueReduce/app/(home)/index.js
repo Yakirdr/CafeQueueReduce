c1682abe405029e4e0f1ac84e3256c7a10587c34
yakir <158854050+YakirDray@users.noreply.github.com>
2024-03-03 14:02:00 +0200

    yakir 1

--- CafeQueueReduce/app/(home)/index.js
+++ CafeQueueReduce/app/(home)/index.js
@@ -1,14 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Carousel from "../../Components/Carousal";
-import softmenu from "../../Components/softmenu";
+import Softmenu from "../../Components/softmenu";
 
 const index = () => {
   return (
     <View>
       <Text>home screen please done!</Text>
     <Carousel/>
-   <Mmenu/>
+   
     </View>
   )
 }
