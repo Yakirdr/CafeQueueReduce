3efccd37cd09042175c4d2e98996c1a25eb94743
yakir <158854050+YakirDray@users.noreply.github.com>
2024-03-04 16:50:55 +0200

    starting back of orders

---
+++ CafeQueueReduce/Redax/ex1.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ex1 = () => {
+  return (
+    <View>
+      <Text>ex1</Text>
+    </View>
+  )
+}
+
+export default ex1
+
+const styles = StyleSheet.create({})
