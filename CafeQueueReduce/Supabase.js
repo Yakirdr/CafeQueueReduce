c5dc5710adbd133cd3542d1f66cacea9e031605a
yakir <158854050+YakirDray@users.noreply.github.com>
2024-03-07 14:03:28 +0200

    אני כמנהל מבצע יצירה ומחיקה של משתמש דרך supabase

--- CafeQueueReduce/Supabase.js
+++ CafeQueueReduce/Supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import "react-native-url-polyfill/auto";
+//our url
 const supabaseUrl = "https://eboziqmbycpfiqlubgre.supabase.co";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVib3ppcW1ieWNwZmlxbHViZ3JlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDc3MzQwOTYsImV4cCI6MjAyMzMxMDA5Nn0.bc4BOwD6Mc2bYlfVuh2QezT6IPiZlCdOLMAmNSfnLcE";
